db20fabd78f876ca1313be746fd711c0bc126de4
ChiaraChiocca <167799487+ChiaraChiocca@users.noreply.github.com>
2024-05-27 15:05:25 -0300

    Add files via upload

---
+++ la-comercial/controladores/articulos.js
@@ -0,0 +1,56 @@
+export const articulos = [
+    {
+        id: 1,
+        codigo: "101",
+        nombre: "iPhone SE"
+        descripción: {
+            procesador: "A13 Bionic",
+            almacenamiento: "128 GB",
+            camaras: "7 Mp / 12 Mp",
+            pantalla: "4,7 IPS Retina"
+        },
+        precio: "74000",
+        imagen: "iPhone-SE-Black.jpg"
+    },
+    {
+        id: 2,
+        codigo: "101",
+        nombre: "iPhone SE"
+        descripción: {
+            procesador: "A13 Bionic",
+            almacenamiento: "128 GB",
+            camaras: "7 Mp / 12 Mp",
+            pantalla: "4,7 IPS Retina"
+        },
+        precio: "74000",
+        imagen: "iPhone-SE-Black.jpg"
+    },
+    {
+        id: 3,
+        codigo: "103",
+        nombre: "iPhone 11 Pro"
+        descripción: {
+            procesador: "A13 Bionic",
+            almacenamiento: "156 GB",
+            camaras: "12 Mp / 12 Mp (Triple)",
+            pantalla: "5,8 Super Retina XDR"
+        },
+        precio: "74000",
+        imagen: "iPhone-SE-Black.jpg"
+    },
+    {
+        id: 4,
+        codigo: "101",
+        nombre: "iPhone SE"
+        descripción: {
+            procesador: "A13 Bionic",
+            almacenamiento: "128 GB",
+            camaras: "7 Mp / 12 Mp",
+            pantalla: "4,7 IPS Retina"
+        },
+        precio: "74000",
+        imagen: "iPhone-SE-Black.jpg"
+    },
+
+
+]
